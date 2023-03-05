1780e300b335546047b419c52ecc50d44eddd951
Fuweimin <fuweimin0000@qq.com>
2023-03-05 19:56:15 +0800

    master commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix2222");
+        System.out.println("hello hot-fix2222");
         FileInputStream fis = null;
         FileOutputStream fos = null;
         try {
